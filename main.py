11d9f7812d4fc720c5433e607c5551c25658b857
CoderSagnik00 <sagnikroycode@gmail.com>
2024-12-01 04:51:13 +0530

    Esha can now track the project desc and path and save it to the project.json for later use

--- main.py
+++ main.py
@@ -1,7 +1,103 @@
+import json
 import re
 from system import System
 
 from esha import Esha
+
+
+def CreateProject():
+    # Regex pattern to extract key-value pairs
+    pattern = r"\{(\w+)=([^\}]+)\}"
+
+    # Find all matches
+    matches = re.findall(pattern, reply)
+
+    # Create a dictionary from the matches
+    project = {key: value for key, value in matches}
+
+    # Access the project details
+    projectName = project.get("projectName")
+    projectDesc = project.get("desc")
+    projectPath = project.get("projectPath")
+
+    print(f"Project Name: {projectName}")
+    print(f"Description: {projectDesc}")
+    print(f"Project Path: {projectPath}")
+    chk = System.CreateFolder(folderName=projectName, path=projectPath)
+    if chk == True:
+        # Adding the project details in project.json to keep a track of it
+        with open("project.json") as file:
+            data = json.load(file)
+        data[projectName] = {"desc": projectDesc, "path": projectPath}
+        with open("project.json", "w") as file:
+            json.dump(data, file, indent=4)
+
+
+        esha.TextToSpeechWithPYttsx3(
+            esha.Brain(
+                "Say Project created if you need any help please ask me or something like that"
+            )
+        )
+    elif chk == "ProjExist":
+        esha.TextToSpeechWithPYttsx3(
+            esha.Brain("Say project already exist or something like that")
+        )
+    elif chk == "PermissionError":
+        esha.TextToSpeechWithPYttsx3(
+            esha.Brain(
+                "Say I don't have proper permision to create a project or something like that"
+            )
+        )
+    elif chk == False:
+        esha.TextToSpeechWithPYttsx3(
+            esha.Brain(
+                "Say Sorry I can't create the project problem occured or something like this"
+            )
+        )
+
+
+def GetProjDet():
+    # Regex pattern to extract the value inside the parentheses
+    pattern = r"\{GetProjDet\((\w+)\)\}"
+
+    # Find the match
+    match = re.search(pattern, reply)
+
+    # Check if a match is found
+    if match:
+        detAbout = match.group(1)
+        print(f"detAbout: {detAbout}")
+        # Regex pattern to extract the value of 'name'
+        pattern = r"\{name=([^\}]+)\}"
+
+        # Find the match
+        match = re.search(pattern, reply)
+
+        # Check if a match is found
+        if match:
+            projName = match.group(1)
+            print(f"Proj Name: {projName}")
+        else:
+            esha.Brain("Say no data found about the project or something like that")
+        with open("project.json") as file:
+            data = json.load(file)
+            if detAbout == "desc":
+                esha.TextToSpeechWithPYttsx3(
+                    esha.Brain(
+                        f"Say description of project is {data[projName]['desc']}"
+                    )
+                )
+            elif detAbout == "path":
+                esha.TextToSpeechWithPYttsx3(
+                    esha.Brain(
+                        f"Say location of project is {data[projName]['path']}"
+                    )
+                )
+    else:
+        esha.TextToSpeechWithPYttsx3(
+            esha.Brain("Say no data found about the project or something like that")
+        )
+
 
 esha = Esha()
 try:
@@ -17,50 +113,10 @@ try:
                 # If it's a system message then Esha won't say it
                 if "{system}" in reply:
                     if "{CreateProject}" in reply:
-                        # Regex pattern to extract key-value pairs
-                        pattern = r"\{(\w+)=([^\}]+)\}"
+                        CreateProject()
+                    if "{GetProjDet" in reply:
+                        GetProjDet()
 
-                        # Find all matches
-                        matches = re.findall(pattern, reply)
-
-                        # Create a dictionary from the matches
-                        projectDesc = {key: value for key, value in matches}
-
-                        # Access the project details
-                        projectName = projectDesc.get("projectName")
-                        desc = projectDesc.get("desc")
-                        projectPath = projectDesc.get("projectPath")
-
-                        print(f"Project Name: {projectName}")
-                        print(f"Description: {desc}")
-                        print(f"Project Path: {projectPath}")
-                        chk = System.CreateFolder(
-                            folderName=projectName, path=projectPath
-                        )
-                        if chk == True:
-                            esha.TextToSpeechWithPYttsx3(
-                                esha.Brain(
-                                    "Say Project created if you need any help please ask me or something like that"
-                                )
-                            )
-                        elif chk == "ProjExist":
-                            esha.TextToSpeechWithPYttsx3(
-                                esha.Brain(
-                                    "Say project already exist or something like that"
-                                )
-                            )
-                        elif chk == "PermissionError":
-                            esha.TextToSpeechWithPYttsx3(
-                                esha.Brain(
-                                    "Say I don't have proper permision to create a project or something like that"
-                                )
-                            )
-                        elif chk == False:
-                            esha.TextToSpeechWithPYttsx3(
-                                esha.Brain(
-                                    "Say Sorry I can't create the project problem occured or something like this"
-                                )
-                            )
                 else:
                     esha.TextToSpeechWithPYttsx3(reply)
 
